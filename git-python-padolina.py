e64b4d6869c11099f0db3c6555e1521013d032b7
PadolinaReinelle <reinellepadolina@gmail.com>
2022-04-13 10:38:45 +0700

    My Educatonal Background added

--- git-python-padolina.py
+++ git-python-padolina.py
@@ -1,3 +1,11 @@
 print ("Reinelle Padolina")
 print ("San Antonio Sta. Cruz MArinduque")
-print ("April 18, 2002")
+print ("April 18 2002")
+
+name_school  = "Marinduque State College"
+course = "Bachelor of Science in Information Technology"
+section = "BSI/T - 2B"
+
+print (name_school)
+print (course)
+print (section)
